6c2066de1f639e40f6d4c70319376591a315a25a
Christian Meyer <55146330+chrismeyer10@users.noreply.github.com>
2025-08-12 22:19:42 +0200

    Use Material3 pull refresh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.32.0")
     implementation("com.google.android.material:material:1.11.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
