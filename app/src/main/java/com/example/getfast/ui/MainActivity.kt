dda5066d7ab87d124b99ffb0582d32b0609f1206
Chris <christian.meyer@iav.de>
2025-08-12 22:36:23 +0200

    experimental

--- app/src/main/java/com/example/getfast/ui/MainActivity.kt
+++ app/src/main/java/com/example/getfast/ui/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -33,6 +34,7 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: ListingViewModel by viewModels()
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val notifier = Notifier(this)
